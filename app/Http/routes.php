3564dc2942b7266d2f242bb5e57118c7fe74f05a
M.T <hunter@cure.104.net>
2018-05-04 06:43:04 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,12 @@
 
 Route::get('/', 'TasksController@index'); 
 
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+
+
 Route::resource('tasks', 'TasksController'); 
 
 //Route::get('/', function () {
